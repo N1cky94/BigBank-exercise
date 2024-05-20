bdf5d63c130841cdad6abf1730cea38aadb7be28
Nicky <nick.bauters@me.com>
2024-05-20 14:06:16 +0200

    Feat: Test the logic in the Main app

--- src/be/archilios/bigBank/BigBankApp.java
+++ src/be/archilios/bigBank/BigBankApp.java
@@ -1,9 +1,20 @@
 package be.archilios.bigBank;
 
+import be.archilios.bigBank.domain.*;
+
 public class BigBankApp {
     
     public static void main(String[] args) {
-    
+        BankAccount acc1 = new BankAccount("BE1234567891234", "NICK BAUTERS", new Euro(250), new AccountTransactions());
+        BankAccount acc2 = new BankAccount("BE1234567895678", "KELLY DE LANGE", new Euro(150), new AccountTransactions());
+        
+        TransferHandler handler = new TransferHandler();
+        handler.transfer(new TransferHandler.TransferCommand(acc1, acc2, new Euro(23.5)));
+        
+        System.out.println(acc1);
+        System.out.println(acc2);
+        
+        
     }
     
 }
